cc9933d5f298214a968203e559386b0b6fd0715c
sidiqbimo <sani21bimo@gmail.com>
2025-02-13 09:58:05 +0700

    bimo start editing

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        '/': (context) => SurveyorProjects(),
+        '/': (context) => Welcome()
       },
     );
   }
