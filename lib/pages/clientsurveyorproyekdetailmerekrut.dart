aaa46c999b5eab9d4186fdd9726b1dc9b915467b
nabilamutiara <116213455+nabilamutiara@users.noreply.github.com>
2025-03-27 19:52:07 +0700

    pop up bagian client

--- lib/pages/clientsurveyorproyekdetailmerekrut.dart
+++ lib/pages/clientsurveyorproyekdetailmerekrut.dart
@@ -670,15 +670,23 @@ class Clientsurveyorproyekdetailmerekrut extends StatelessWidget {
                       child: Icon(Icons.task, color: Color(0xFF826754)),
                     ),
                     SizedBox(width: 10),
-                    Container(
-                      width: 40,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Color(0xFF826754),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                        Future.delayed(Duration.zero, () {
+                          _rekrutpelamarini(context); // Tampilkan pop-up kedua
+                        });
+                      },
+                      child: Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color(0xFF826754),
+                        ),
+                        child: Icon(Icons.arrow_forward_rounded, color: Color(0xFFF1E9E5)),
                       ),
-                      child: Icon(Icons.arrow_forward_rounded, color: Color(0xFFF1E9E5)),
-                    )
+                    ),
                   ],
                 ),
                 Row(
@@ -1104,5 +1112,397 @@ class Clientsurveyorproyekdetailmerekrut extends StatelessWidget {
       },
     );
   }
+
+  void _rekrutpelamarini(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+      ),
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return ClipRRect(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.all(0),
+            color: Color(0xFFF1E9E5),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(height: 24),
+                Divider(
+                  thickness: 3,
+                  indent: 150,
+                  endIndent: 150,
+                  color: Color(0xFFB0B0B0),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Rekrut Pelamar Ini?",
+                  style: TextStyle(
+                    fontFamily: 'SourceSans3',
+                    fontSize: 24,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF705D54),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Stack(
+                  alignment: Alignment.center,
+                  clipBehavior: Clip.none,
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage('assets/images/hermanwalton.png'),
+                    ),
+                    Positioned(
+                      bottom: -3,
+                      child: Material(
+                        color: Colors.transparent,
+                        child: Container(
+                          width: 45,
+                          height: 15,
+                          decoration: BoxDecoration(
+                            color: Color(0xFFD7CCC8),
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: Color(0xFF826754),
+                              width: 1,
+                            ),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.thumb_up,
+                                color: Color(0xFF826754),
+                                size: 8,
+                              ),
+                              SizedBox(width: 2),
+                              Text(
+                                "90%",
+                                style: TextStyle(
+                                  fontFamily: 'NunitoSans',
+                                  fontSize: 8,
+                                  fontWeight: FontWeight.w700,
+                                  color: Color(0xFF826754),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 12),
+                Text(
+                  "Herman Walton",
+                  style: TextStyle(
+                    fontFamily: 'NunitoSans',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xFF705D54),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  padding: EdgeInsets.all(30),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF826754),
+
+                  ),
+                  child: Row(
+                    children: [
+
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                            Future.delayed(Duration.zero, () {
+                              _apakahandayakinmerekrutpelamarini(context); // Tampilkan pop-up kedua
+                            });
+                          },
+                          child: Container(
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              color: Color(0xFFF1E9E5),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.account_box,
+                                  color: Color(0xFF826754),
+                                  size: 24,
+                                ),
+                                SizedBox(width: 5),
+                                Text(
+                                  "Ya, rekrut",
+                                  style: TextStyle(
+                                    color: Color(0xFF826754),
+                                    fontFamily: "NunitoSans",
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 5),
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+
+                            borderRadius: BorderRadius.circular(8),
+                            border: Border.all(color: Color(0xFFF1E9E5), width: 1),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.not_interested,
+                                color: Color(0xFFF1E9E5),
+                                size: 24,
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                "Tolak",
+                                style: TextStyle(
+                                  color: Color(0xFFF1E9E5),
+                                  fontFamily: "NunitoSans",
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+
+                      ),
+
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _apakahandayakinmerekrutpelamarini(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+      ),
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return ClipRRect(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.all(0),
+            color: Color(0xFFF1E9E5),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(height: 24),
+                Divider(
+                  thickness: 3,
+                  indent: 150,
+                  endIndent: 150,
+                  color: Color(0xFFB0B0B0),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 24),
+                  child: Text.rich(
+                    TextSpan(
+                      text: "Apakah Anda yakin ",
+                      style: TextStyle(
+                        fontFamily: "SourceSans3",
+                        color: Color(0xFF705D54),
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: "merekrut ",
+                          style: TextStyle(
+                            fontFamily: "SourceSans3",
+                            color: Color(0xFF705D54),
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        TextSpan(
+                          text: "pelamar ini?",
+                          style: TextStyle(
+                            fontFamily: "SourceSans3",
+                            color: Color(0xFF705D54),
+                            fontSize: 20,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+
+                SizedBox(height: 20),
+                Stack(
+                  alignment: Alignment.center,
+                  clipBehavior: Clip.none,
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage('assets/images/hermanwalton.png'),
+                    ),
+                    Positioned(
+                      bottom: -3,
+                      child: Material(
+                        color: Colors.transparent,
+                        child: Container(
+                          width: 45,
+                          height: 15,
+                          decoration: BoxDecoration(
+                            color: Color(0xFFD7CCC8),
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: Color(0xFF826754),
+                              width: 1,
+                            ),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.thumb_up,
+                                color: Color(0xFF826754),
+                                size: 8,
+                              ),
+                              SizedBox(width: 2),
+                              Text(
+                                "90%",
+                                style: TextStyle(
+                                  fontFamily: 'NunitoSans',
+                                  fontSize: 8,
+                                  fontWeight: FontWeight.w700,
+                                  color: Color(0xFF826754),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 12),
+                Text(
+                  "Herman Walton",
+                  style: TextStyle(
+                    fontFamily: 'NunitoSans',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xFF705D54),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  padding: EdgeInsets.all(30),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF826754),
+
+                  ),
+                  child: Row(
+                    children: [
+
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                            Future.delayed(Duration.zero, () {
+                              _rekrutpelamarini(context); // Tampilkan pop-up kedua
+                            });
+                          },
+                          child: Container(
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              color: Color(0xFFF1E9E5),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.account_box,
+                                  color: Color(0xFF826754),
+                                  size: 24,
+                                ),
+                                SizedBox(width: 5),
+                                Text(
+                                  "Ya, rekrut",
+                                  style: TextStyle(
+                                    color: Color(0xFF826754),
+                                    fontFamily: "NunitoSans",
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 5),
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+
+                            borderRadius: BorderRadius.circular(8),
+                            border: Border.all(color: Color(0xFFF1E9E5), width: 1),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.not_interested,
+                                color: Color(0xFFF1E9E5),
+                                size: 24,
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                "Tolak",
+                                style: TextStyle(
+                                  color: Color(0xFFF1E9E5),
+                                  fontFamily: "NunitoSans",
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+
+                      ),
+
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
 
 }
